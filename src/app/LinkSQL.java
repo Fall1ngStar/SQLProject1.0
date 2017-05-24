3d5772a8cbb3ebb46424dc669497085a435cd2ae
Bloody <patrickdu3e@hotmail.fr>
2017-05-24 13:59:50 +0200

    Ajout fonction connection/requete

--- src/app/LinkSQL.java
+++ src/app/LinkSQL.java
@@ -3,5 +3,28 @@ package app; /**
  */
 import java.sql.*;
 public class LinkSQL {
-    
+    private Connection con;
+    private LinkSQL(){
+
+    }
+    private static LinkSQL INSTANCE = new LinkSQL();
+    public static LinkSQL getInstance()
+    {
+        return INSTANCE;
+    }
+    public void connexionServeur(String nomUtilisateur,String MotDePasse){
+
+        try {
+            con = DriverManager.getConnection("jdbc:oracle:thin:@iutdoua-oracle.univ-lyon1.fr:1521:orcl",nomUtilisateur,MotDePasse );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void executeRequete(String requete) {
+        ResultSet rs = stmt.executeQuery
+                (requete);
+        while (rs.next()) {
+            System.out.println(rs.getType(#id_col));
+        }
+    }
 }
